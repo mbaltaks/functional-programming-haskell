e8c4cea2ee61782e968d04211f118f41b09a22dc
Michael Baltaks <m@baltaks.com>
2019-09-23 10:27:12 +1000

    Chapter 2.10

--- haskell-book/src/chapter02/test.hs
+++ haskell-book/src/chapter02/test.hs
@@ -1,11 +1,15 @@
 module Test where
 
 sayHello :: String -> IO ()
-sayHello x =
-    putStrLn("Hello, " ++ x ++ "!")
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
 
 triple x = x * 3
 
 approximateAreaOfCircleWithRadius radius = 3.14 * (radius * radius)
 
 areaOfCircleWithRadius radius = pi * (radius * radius)
+
+mult1 = x * y
+  where
+    x = 5
+    y = 6
